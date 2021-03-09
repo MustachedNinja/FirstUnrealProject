d0971a9562dbaf21d3cc1027480a296b3b8391bd
MustachedNinja <kostya.yatsuk9@live.com>
2021-03-09 00:59:37 -0800

    Add character movement

--- Source/FirstProject/BatteryMan.h
+++ Source/FirstProject/BatteryMan.h
@@ -5,6 +5,13 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Blueprint/UserWidget.h"
 #include "BatteryMan.generated.h"
 
 UCLASS()
@@ -12,9 +19,22 @@ class FIRSTPROJECT_API ABatteryMan : public ACharacter
 {
 	GENERATED_BODY()
 
+
+
 public:
 	// Sets default values for this character's properties
 	ABatteryMan();
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		UCameraComponent* FollowCamera;
+
+	void MoveForward(float Axis);
+	void MoveRight(float Axis);
+	
+	bool bDead;
+
 
 protected:
 	// Called when the game starts or when spawned
